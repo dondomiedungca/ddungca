5a8070a944f8e30aecbe47173d920e1039578faa
Dondomie Dungca <41827126+nodnod08@users.noreply.github.com>
2023-09-22 01:06:00 +0800

    Update the api name

--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,10 +1,7 @@
 import { NextApiResponse, NextApiRequest } from "next";
 import axios from "axios";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function POST(req: NextApiRequest, res: NextApiResponse) {
   const { email, message, name } = JSON.parse(req.body);
 
   const response = await axios.post(`${process.env.BACKEND_API}/mail`, {
